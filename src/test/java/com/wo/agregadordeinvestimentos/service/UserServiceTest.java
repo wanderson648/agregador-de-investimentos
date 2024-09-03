be51c0b5f44671d32bbf1ebde0471f65f505388e
wanderson648 <testepedroca@gmail.com>
2024-09-03 11:28:12 -0300

    cria test unitario para createUser

---
+++ src/test/java/com/wo/agregadordeinvestimentos/service/UserServiceTest.java
@@ -0,0 +1,89 @@
+package com.wo.agregadordeinvestimentos.service;
+
+import com.wo.agregadordeinvestimentos.controller.CreateUserDto;
+import com.wo.agregadordeinvestimentos.entity.User;
+import com.wo.agregadordeinvestimentos.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Captor
+    private ArgumentCaptor<User> userArgumentCaptor;
+
+    @Nested
+    class createUser {
+
+        @Test
+        @DisplayName("Should create a user with success")
+        void shouldCreateAUserWithSuccess() {
+            // Arrange
+            var user = new User(
+                    UUID.randomUUID(),
+                    "username",
+                    "email@email.com",
+                    "password",
+                    Instant.now(),
+                    null
+            );
+
+            doReturn(user).when(userRepository).save(userArgumentCaptor.capture());
+
+            // Act
+            var input = new CreateUserDto("username",
+                    "email@email.com", "123");
+
+            var output = userService.crateUser(input);
+
+            // Assert
+            assertNotNull(output);
+
+            var userCaptured = userArgumentCaptor.getValue();
+
+            assertEquals(input.username(), userCaptured.getUsername());
+            assertEquals(input.email(), userCaptured.getEmail());
+            assertEquals(input.password(), userCaptured.getPassword());
+
+        }
+
+        @Test
+        @DisplayName("Should throw exception when error occurs")
+        void shouldThrowExceptionWhenErrorOccurs() {
+
+            // Arrange
+            doThrow(new RuntimeException()).when(userRepository).save(any());
+            var input = new CreateUserDto(
+                    "username",
+                    "email@email.com",
+                    "123"
+            );
+
+            // Act & Assert
+            assertThrows(RuntimeException.class, () -> userService.crateUser(input));
+        }
+    }
+
+
+}
